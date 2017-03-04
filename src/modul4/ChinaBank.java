1c597420245bf245a13d8b034f15e789a3d9eb49
Shyrick <Lykun@mail.ru>
2017-03-04 14:41:14 +0300

    Homework 4 Task 4_1

---
+++ src/modul4/ChinaBank.java
@@ -0,0 +1,30 @@
+package modul4;
+
+import java.util.Currency;
+
+public class ChinaBank extends Bank {
+
+    public ChinaBank(Long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
+
+    @Override
+    int getLimitOfWithdrawal() {
+        return 0;
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        return 0;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        return 0;
+    }
+
+    @Override
+    int getCommission(int summ) {
+        return 0;
+    }
+}
