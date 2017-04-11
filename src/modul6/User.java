b7009ccc47e7d94ea530654aa03d273ab7258a8c
Shyrick <Lykun@mail.ru>
2017-04-11 21:53:08 +0300

    Homework 6 Task 6_2

---
+++ src/modul6/User.java
@@ -0,0 +1,38 @@
+package modul6;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private int balance;
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
